79afc86c8b0559ab1c47779edfcf5cf44b54dbed
undeadko <undeadko@abv.bg>
2022-08-04 19:06:17 +0300

    Lesson 3

---
+++ 3.MultipleAssignment/MultipleAssignment.py
@@ -0,0 +1,19 @@
+# Multiple assignment is when multiple variables are assigned multiple values on one line.
+
+# Normal assignment.
+name = "John";
+
+# Multiple assignment.
+name, age, attractive = "Johnny", 25, True;
+
+print(name);
+print(age);
+print(attractive);
+
+# If the variables are all the same value we can do this.
+age1 = age2 = age3 = age4 = 30;
+
+print(age1);
+print(age2);
+print(age3);
+print(age4);
